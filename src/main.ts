e3b69b579ff1815428283b45b89311e2126f4cd4
Cerciber <cerciber@hotmail.com>
2024-06-06 08:20:53 -0500

    feat: Auto doc with swagger advances

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest General Architecture')
+    .setDescription(
+      'General architecture for Nest.js with TypeScript implementing 3 Layered Architecture. Node.js.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
